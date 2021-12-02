dfbc5cfc60d1e24bf9c95124e6144383e2549b53
Kidkat <kidkat.co.id@gmail.com>
2021-12-02 15:39:04 +0700

    Menambahkan (getElementsByClassName)

--- script.js
+++ script.js
@@ -6,3 +6,7 @@ judul.style.color = 'red';
 // // document.getElementsByTagName()
 const p = document.getElementsByTagName('p');
 p[0].style.backgroundColor ="red";
+
+// document.getElementsByClassName()
+const p1 = document.getElementsByClassName('p1');
+p1.style.color = "red";
